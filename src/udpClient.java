c3acfbc1a758fe81294a154276ec508c27e98ca9
Tyler Kaelin <tylerkaelin18@icloud.com>
2021-03-15 17:05:28 -0500

    added public ip address functionality to work over the internet

--- src/udpClient.java
+++ src/udpClient.java
@@ -1,9 +1,9 @@
 //Tyler Kaelin and Logan Morris
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.*;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class udpClient
 {
@@ -14,13 +14,14 @@ public class udpClient
 
         Scanner clientInput = new Scanner(System.in);
 
-        // String logansIpTextRepresentation = "192.168.1.148";
-
         //My machines local ip
-        InetAddress ip = InetAddress.getLocalHost();
-        //InetAddress logansIP = InetAddress.getByName(logansIpTextRepresentation);
+        //InetAddress ip = InetAddress.getLocalHost();
+        //InetAddress myIp = InetAddress.getByName();
 
-        //InetAddress logansIP = ip.getByName(logansIpTextRepresentation);
+        //InetAddress logansIP = ip.getByName(myIP);
+
+
+        InetAddress myIp = InetAddress.getByName(GetPublicIp());
 
 
         byte[] messageToBeSent;
@@ -32,7 +33,7 @@ public class udpClient
             messageToBeSent = messageBeforeByteConversion.getBytes(); //Turning message into byte representation
 
             DatagramPacket DpSend =
-                    new DatagramPacket(messageToBeSent, messageToBeSent.length, ip, 1234);
+                    new DatagramPacket(messageToBeSent, messageToBeSent.length, myIp, 1234);
 
             socketToTransmitData.send(DpSend);
 
@@ -41,5 +42,25 @@ public class udpClient
             }
 
         }
+    }
+
+    public static String GetPublicIp()
+    {
+        String publicIpAddress = "";
+        try
+        {
+            URL urlName = new URL("http://bot.whatismyipaddress.com");
+
+            BufferedReader sc = new BufferedReader(new InputStreamReader(urlName.openStream()));
+
+            publicIpAddress = sc.readLine().trim();
+        }
+        catch (Exception e)
+        {
+            publicIpAddress = "Cannot Execute Properly";
+        }
+
+        // System.out.println("Public IP Address: " + systemipaddress +"\n");
+        return publicIpAddress;
     }
 }
